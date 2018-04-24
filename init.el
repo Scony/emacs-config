c4165af3cb57d5f02944b22032020eba28a83753
Pawel Lampe <pawel.lampe@nokia.com>
2018-04-24 13:06:11 +0200

    update

--- init.el
+++ init.el
@@ -5,4 +5,8 @@
 (require 'google-c-style)
 (add-hook 'c-mode-common-hook 'google-set-c-style)
 
+(require 'better-defaults)
+(require 'loccur)
+(require 'fill-column-indicator)
+
 (setq dummy-string "it works!")
